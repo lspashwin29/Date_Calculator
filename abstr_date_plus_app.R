86d3a6a18fb1de9204d1ea7a77de23f00ce995b5
Ashwin Subramanian Lakshmanan <ashwin2@ncsu.edu>
2017-12-07 12:21:29 -0500

    Update abstr_date_plus_app.R

--- abstr_date_plus_app.R
+++ abstr_date_plus_app.R
@@ -4,12 +4,6 @@ library(shinythemes)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-  # fluidRow(column(width = 4), column(width = 2, offset = 3), h1("COTA: Date Calculator")),
-  
-  
-  
-  # htmlOutput("logo"),
   
   headerPanel(h1("COTA: Date Calculator")),
   
@@ -31,12 +25,7 @@ ui <- fluidPage(
     
    theme = shinytheme("slate"),
     
-  # shinythemes::themeSelector(),
-  
-  # mainPanel(
-    # textOutput("heading"),
-  # ),
-  
+
   mainPanel(flowLayout(
     dateInput('date_entered', 'Enter your start date', value = Sys.Date(), startview = "decade"),
     # sliderInput('length_entered', 'How many days forward?', 0, 1000, 20),
@@ -47,14 +36,6 @@ ui <- fluidPage(
     tags$style(type='text/css', "#calculated_date { width:60%; vertical-align:bottom; margin-top: 26px;}"),
     tags$style(type='text/css', "#go { width:60%; vertical-align:bottom; margin-top: 26px;}")))
   
-  # mainPanel(
-  #   verbatimTextOutput("calculated_date"),
-  # 
-  # tags$style(type='text/css', "#calculated_date { width:9%; vertical-align:bottom; margin-top: 250px;}"),
-  # tags$style(type='text/css', "#go { width:60%; vertical-align:bottom; margin-top: 26px;}")
-  # # tags$style(type='text/css', "#length_entered { width:9%; vertical-align:bottom; margin-top: 250px;}")
-  # 
-  # )
   
 )
 
@@ -74,18 +55,11 @@ server <- function(input, output) {
   
   output$calculated_date <- renderText({temp()})
   
-  # output$calculated_date <- renderText({
-  #   isolate(
-  #   as.character(input$date_entered + input$length_entered)
-  # )
-  # })
   
   output$heading <- renderText({
     as.character("COTA - Date Calculator")
   })
   
-  # src = "https://theWeb/aPictureSomewhere.jpg"
-  # src = "https://res.cloudinary.com/crunchbase-production/image/upload/v1487275519/czcgzvchyjgnkfwlan6o.png"
   src = "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAc1AAAAJGZhYWU2OGJhLWJkYzAtNDgxNC1iMGQ1LWRkYmQ0ZmU2ZjYzNg.png"
   output$logo <- renderText({c('<img src="',src,'">')})
     
